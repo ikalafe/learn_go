9c225cc53b1eaebca6bf25890d74e64aca28e50f
ikalafe <imkalafe@gmail.com>
2025-07-03 07:40:29 -0400

    Refactor main to show how function variables can point to different functions with the same signature

--- ch5/example.go
+++ ch5/example.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var myFuncVariable func(string) int
+	myFuncVariable = f1
+	result := myFuncVariable("Hello")
+	fmt.Println(result)
+
+	myFuncVariable = f2
+	result = myFuncVariable("Hello")
+	fmt.Println(result)
+}
+
+func f1(a string) int {
+	return len(a)
+}
+
+func f2(a string) int {
+	total := 0
+	for _, v := range a {
+		total += int(v)
+	}
+
+	return total
 }
