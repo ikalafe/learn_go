3766e0ab4c51a3c99ac69eceae400d2771cc65b5
ikalafe <imkalafe@gmail.com>
2025-07-04 07:19:00 -0400

    Add Go example showing how maps and slices behave when modified inside functions

---
+++ ch5/pass_map_slice.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[int]string{
+		1: "first",
+		2: "second",
+	}
+	modMap(m)
+	fmt.Println(m)
+
+	s := []int{1, 2, 3}
+	modSlice(s)
+	fmt.Println(s)
+}
+
+func modMap(m map[int]string) {
+	m[2] = "Hello"
+	m[3] = "Goodbye"
+	delete(m, 1)
+}
+
+func modSlice(s []int) {
+	for k, v := range s {
+		s[k] = v * 2
+	}
+	s = append(s, 10)
+}
